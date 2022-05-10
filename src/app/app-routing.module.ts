f855c1c679b08c95c1bfc46fdb3a77cec5dd8058
johan.ramirez <johan.ramirez@sofka.com.co>
2022-05-10 12:47:05 -0500

    Added: app scafold and basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: '404',
+    component: ErrorPageComponent,
+  },
+  {
+    path: 'heroes',
+    loadChildren: () =>
+      import('./heroes/heroes-routing.module').then(
+        (m) => m.HeroesRoutingModule
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: '404',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
